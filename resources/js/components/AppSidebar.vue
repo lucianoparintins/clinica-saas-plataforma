ccfb782cf1984f561ca3a2e35c6a794d436a2c42
Luciano Parintins Viana <luciano.parintins@gmail.com>
2026-03-15 21:21:51 -0300

    feat: atualiza branding, nome do app e traduz termos da navegação

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -39,16 +39,7 @@ const mainNavItems = computed<NavItem[]>(() => [
 ]);
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: FolderGit2,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
+
 ];
 </script>
 
